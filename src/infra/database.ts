455444bc2fede12170eb7728af26e5f3b8c385a9
franklinhelio00 <franklinhelio00@gmail.com>
2023-10-07 14:31:35 -0300

    create event useCase

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -6,7 +6,7 @@ export async function connect(){
 mongoose.connect('mongodb+srv://franklinhelio00:159753MongooseDb@ticketdb.nu3v3po.mongodb.net/hero-tickets');`
 is connecting to a MongoDB database using the Mongoose library. It is using the `connect` method of
 the `mongoose` object to establish a connection to the specified MongoDB database URL. */
-        await mongoose.connect('mongodb+srv://franklinhelio00:159753MongooseDb@ticketdb.nu3v3po.mongodb.net/hero-tickets');
+        await mongoose.connect('CAMINHO DO MONGO');
         console.log('connect database success');
         
     } catch (error) {
